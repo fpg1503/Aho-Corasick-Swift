a8c88b816489f15be906040b6963e942dfad3cc1
Ruslan Moskalenko <r.moskalenko@olearis.com>
2021-09-14 11:27:03 +0300

    Package: updated path

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "Aho-Corasick-Swift",
-            path: "Aho-Corasick-Swift",
-            publicHeadersPath: "."
+            path: "Source"
         ),
     ]
 )
