2d844f9347f36391d4c1a97fb0714b1a2ef88f43
Ruslan Moskalenko <r.moskalenko@olearis.com>
2021-09-14 10:34:39 +0300

    Added package file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,24 @@
 import PackageDescription
 
 let package = Package(
-  name: "Aho-Corasick"
+    name: "Aho-Corasick-SPM",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "Aho-Corasick-SPM",
+            targets: ["Aho-Corasick-SPM"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "Aho-Corasick-SPM",
+            dependencies: []),
+        .testTarget(
+            name: "Aho-Corasick-SPM",
+            dependencies: ["Aho-Corasick-SPM"]),
+    ]
 )
